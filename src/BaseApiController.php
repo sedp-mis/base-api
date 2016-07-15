37c9a373ee29162580755683ed397eede1d087b9
markii1607 <mjbundalian61@gmail.com>
2016-07-15 16:50:56 +0800

    initial commit for base api controller

---
+++ src/BaseApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+class BaseApiController {
+
+  public function index(){
+
+  }
+
+  public function show($id){
+
+  }
+
+  public function store(){
+
+  }
+
+  public function update($id){
+
+  }
+
+  public function destroy($id){
+
+  }
+  
+}
