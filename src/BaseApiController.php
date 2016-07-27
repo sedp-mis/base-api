51524febdadb4ccbce8ceb5c660335843412b443
markii1607 <mjbundalian61@gmail.com>
2016-07-27 20:46:46 +0800

    add controller baseApi

--- src/BaseApiController.php
+++ src/BaseApiController.php
@@ -1,25 +1,38 @@
 <?php
 
-class BaseApiController {
+namespace SedpMis\BaseApi;
 
-  public function index(){
+class BaseApiController extends \Illuminate\Routing\Controller
+{
 
+  /**
+   * @var \SedpMis\BaseRepository\RepositoryInterface
+   */
+  protected $repo;
+
+  public function index()
+  {
+    return $this->repo->all();
   }
 
-  public function show($id){
-
+  public function show($id)
+  {
+    return $this->repo->find($id);
   }
 
-  public function store(){
-
+  public function store()
+  {
+    return $this->repo->create();
   }
 
-  public function update($id){
-
+  public function update($id)
+  {
+    return $this->repo->update($id);
   }
 
-  public function destroy($id){
-
+  public function destroy($id)
+  {
+    return $this->repo->delete($id);
   }
   
 }
