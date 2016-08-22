65eec32466fc3c7409acd90c848e890ec5bc1f26
arjon_jason_castro <ajcastro29@gmail.com>
2016-08-22 11:31:47 +0800

    Add db creation in setup. Add local PdoExec

--- test_classes/TestCase.php
+++ test_classes/TestCase.php
@@ -15,4 +15,11 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../bootstrap/start.php';
 	}
+
+	public function setUp()
+	{
+		(new PdoExec)->execute('create database if not exists base_api_test_db');
+
+		parent::setUp();
+	}
 }
