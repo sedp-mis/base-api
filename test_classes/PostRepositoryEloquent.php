c9d1749bc0c64be55728793b2cc1fae892a75819
Arjon Jason Castro <ajcastro29@gmail.com>
2016-08-28 10:46:04 +0800

    Remove development files. Only src files and other important stuff are included in master for release

--- test_classes/PostRepositoryEloquent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class PostRepositoryEloquent extends \SedpMis\BaseRepository\BaseRepositoryEloquent implements \SedpMis\BaseRepository\RepositoryInterface
-{
-  public function __construct(Post $model)
-  {
-    $this->model = $model;
-  }
-}
